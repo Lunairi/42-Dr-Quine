ed1b4270779af0d1d2d502232ae8bb4c5d35a978
Lunairi <mizukori250@gmail.com>
2020-01-07 14:38:00 -0800

    Oroboros grace done

--- Oroboros/Grace.py
+++ Oroboros/Grace.py
@@ -1,7 +1,7 @@
-# Self printing python
+# Python to Ruby Oroboros
 
 SOURCE = [
-	'# Self printing python',
+	'# Python to Ruby Oroboros',
 	'',
 	'SOURCE = [',
 	']',
@@ -12,19 +12,36 @@ SOURCE = [
 	'	x.write(("{0}{1}{2}".format(chr(39), chr(44), chr(10))))',
 	'',
 	'MAIN = lambda x: ',
-	'	x.write(("{0}".format(chr(10))).join(SOURCE[0:3])) and ',
-	'	WRITESOURCE(x, 0, 24) and ',
-	'	x.write(("{0}".format(chr(10))).join(SOURCE[3:5])) and ',
-	'	x.write("{0}".format(chr(10))) and ',
-	'	x.write(("{0}{1}".format(chr(92), chr(10))).join(SOURCE[5:9])) and ',
-	'	x.write("{0}{0}".format(chr(10))) and ',
-	'	x.write(("{0}{1}".format(chr(92), chr(10))).join(SOURCE[10:22])) and ',
-	'	x.write("{0}{0}".format(chr(10))) and ',
-	'	x.write(SOURCE[23]) and ',
+	'	x.write(("{0}".format(chr(10))).join(SOURCE[18:21])) and ',
+	'	WRITESOURCE(x, 0, 41) and ',
+	'	x.write(("{0}".format(chr(10))).join(SOURCE[21:41])) and ',
 	'	x.write("{0}".format(chr(10))) and ',
 	'	x.close()',
 	'',
 	'MAIN(open("Grace_kid.py", "w+")) ',
+	'# Ruby to Python Oroboros',
+	'',
+	'SOURCE = [',
+	']',
+	'',
+	'WRITESOURCE = -> (x, y, z, a) {',
+	'	a < 2 ? (SOURCE[y..z].each { |b| a == 0 ? (x.puts "#{b}#{92.chr}") : (x.puts "	#{39.chr}#{b}#{39.chr},")} ) ',
+	'		: (SOURCE[y..z].each { |b| x.puts b } )',
+	'}',
+	'',
+	'MAIN = -> {',
+	'	file = File.open("Grace_kid.rb", "w")',
+	'	WRITESOURCE.call(file, 0, 2, 2)',
+	'	WRITESOURCE.call(file, 0, 40, 1)',
+	'	WRITESOURCE.call(file, 3, 4, 2)',
+	'	WRITESOURCE.call(file, 5, 7, 0)',
+	'	WRITESOURCE.call(file, 8, 9, 2)',
+	'	WRITESOURCE.call(file, 10, 14, 0)',
+	'	WRITESOURCE.call(file, 15, 17, 2)',
+	'	file.close',
+	'}',
+	'',
+	'MAIN.call',
 ]
 
 WRITESOURCE = lambda x, y, z: \
@@ -33,15 +50,9 @@ WRITESOURCE = lambda x, y, z: \
 	x.write(("{0}{1}{2}".format(chr(39), chr(44), chr(10))))
 
 MAIN = lambda x: \
-	x.write(("{0}".format(chr(10))).join(SOURCE[0:3])) and \
-	WRITESOURCE(x, 0, 24) and \
-	x.write(("{0}".format(chr(10))).join(SOURCE[3:5])) and \
-	x.write("{0}".format(chr(10))) and \
-	x.write(("{0}{1}".format(chr(92), chr(10))).join(SOURCE[5:9])) and \
-	x.write("{0}{0}".format(chr(10))) and \
-	x.write(("{0}{1}".format(chr(92), chr(10))).join(SOURCE[10:22])) and \
-	x.write("{0}{0}".format(chr(10))) and \
-	x.write(SOURCE[23]) and \
+	x.write(("{0}".format(chr(10))).join(SOURCE[18:21])) and \
+	WRITESOURCE(x, 0, 41) and \
+	x.write(("{0}".format(chr(10))).join(SOURCE[21:41])) and \
 	x.write("{0}".format(chr(10))) and \
 	x.close()
 
--- Oroboros/Grace.rb
+++ Oroboros/Grace.rb
@@ -1,41 +1,63 @@
-# Self printing Ruby
+# Ruby to Python Oroboros
 
 SOURCE = [
-	'# Self printing Ruby',
+	'# Python to Ruby Oroboros',
+	'',
+	'SOURCE = [',
+	']',
+	'',
+	'WRITESOURCE = lambda x, y, z: ',
+	'	x.write("{0}	{1}".format(chr(10), chr(39))) and ',
+	'	x.write(("{0}{1}{2}	{0}".format(chr(39), chr(44), chr(10))).join(SOURCE[y:z])) and ',
+	'	x.write(("{0}{1}{2}".format(chr(39), chr(44), chr(10))))',
+	'',
+	'MAIN = lambda x: ',
+	'	x.write(("{0}".format(chr(10))).join(SOURCE[18:21])) and ',
+	'	WRITESOURCE(x, 0, 41) and ',
+	'	x.write(("{0}".format(chr(10))).join(SOURCE[21:41])) and ',
+	'	x.write("{0}".format(chr(10))) and ',
+	'	x.close()',
+	'',
+	'MAIN(open("Grace_kid.py", "w+")) ',
+	'# Ruby to Python Oroboros',
 	'',
 	'SOURCE = [',
 	']',
 	'',
 	'WRITESOURCE = -> (x, y, z, a) {',
-	'	SOURCE[y..z].each { |b|',
-	'		a == 0 ? (x.puts b) : (x.puts "	#{39.chr}#{b}#{39.chr},")',
-	'	}',
+	'	a < 2 ? (SOURCE[y..z].each { |b| a == 0 ? (x.puts "#{b}#{92.chr}") : (x.puts "	#{39.chr}#{b}#{39.chr},")} ) ',
+	'		: (SOURCE[y..z].each { |b| x.puts b } )',
 	'}',
 	'',
 	'MAIN = -> {',
 	'	file = File.open("Grace_kid.rb", "w")',
-	'	WRITESOURCE.call(file, 0, 2, 0)',
-	'	WRITESOURCE.call(file, 0, 19, 1)',
-	'	file.puts "	#{39.chr}#{SOURCE[20]}#{39.chr}"',
-	'	WRITESOURCE.call(file, 3, 20, 0)',
+	'	WRITESOURCE.call(file, 0, 2, 2)',
+	'	WRITESOURCE.call(file, 0, 40, 1)',
+	'	WRITESOURCE.call(file, 3, 4, 2)',
+	'	WRITESOURCE.call(file, 5, 7, 0)',
+	'	WRITESOURCE.call(file, 8, 9, 2)',
+	'	WRITESOURCE.call(file, 10, 14, 0)',
+	'	WRITESOURCE.call(file, 15, 17, 2)',
 	'	file.close',
 	'}',
 	'',
-	'MAIN.call'
+	'MAIN.call',
 ]
 
 WRITESOURCE = -> (x, y, z, a) {
-	SOURCE[y..z].each { |b|
-		a == 0 ? (x.puts b) : (x.puts "	#{39.chr}#{b}#{39.chr},")
-	}
+	a < 2 ? (SOURCE[y..z].each { |b| a == 0 ? (x.puts "#{b}#{92.chr}") : (x.puts "	#{39.chr}#{b}#{39.chr},")} ) 
+		: (SOURCE[y..z].each { |b| x.puts b } )
 }
 
 MAIN = -> {
 	file = File.open("Grace_kid.rb", "w")
-	WRITESOURCE.call(file, 0, 2, 0)
-	WRITESOURCE.call(file, 0, 19, 1)
-	file.puts "	#{39.chr}#{SOURCE[20]}#{39.chr}"
-	WRITESOURCE.call(file, 3, 20, 0)
+	WRITESOURCE.call(file, 0, 2, 2)
+	WRITESOURCE.call(file, 0, 40, 1)
+	WRITESOURCE.call(file, 3, 4, 2)
+	WRITESOURCE.call(file, 5, 7, 0)
+	WRITESOURCE.call(file, 8, 9, 2)
+	WRITESOURCE.call(file, 10, 14, 0)
+	WRITESOURCE.call(file, 15, 17, 2)
 	file.close
 }
 
